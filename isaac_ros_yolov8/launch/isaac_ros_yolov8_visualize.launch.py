322fe2cc328e0b7a58fccb9b444b604f5b0bebbe
SamuelFoo <fooenzesamuel@gmail.com>
2024-02-14 17:56:08 +0800

    Edit namespace

--- isaac_ros_yolov8/launch/isaac_ros_yolov8_visualize.launch.py
+++ isaac_ros_yolov8/launch/isaac_ros_yolov8_visualize.launch.py
@@ -19,27 +19,21 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import GroupAction, IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import Node, PushRosNamespace
 
 
 def generate_launch_description():
     my_package_dir = get_package_share_directory("isaac_ros_yolov8")
     return LaunchDescription(
         [
-            GroupAction(
-                actions=[
-                    PushRosNamespace("left"),
-                    IncludeLaunchDescription(
-                        PythonLaunchDescriptionSource(
-                            [
-                                os.path.join(my_package_dir, "launch"),
-                                "/yolov8_tensor_rt.launch.py",
-                            ]
-                        )
-                    ),
-                ]
-            )
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    [
+                        os.path.join(my_package_dir, "launch"),
+                        "/yolov8_tensor_rt.launch.py",
+                    ]
+                )
+            ),
         ]
     )
